89197ea711db92ac61fc6815abe97e41c1571cbe
h8d13 <hadean-eon-dev@proton.me>
2026-03-03 22:55:00 +0100

    rem

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 # GET INITIAL OWNER TOKEN
 # docker logs -f sharkord
 
@@ -9,11 +8,3 @@
 
 # convenience wrapper for prod builds
 SHARKORD_AUTOUPDATE=false docker compose --profile prod down && docker compose --profile prod up --build -d #--no-cache
-
-#### NOTES
-
-# Allow embeds on client side (but requires a click on an arrow facing down) DONE
-# User the username provided as the original username (Not SharkordUser) DONE
-# Search in server text channels DONE
-# Direct replies 
-# Fix whiteboard lines should just be point a click, point b click. 
