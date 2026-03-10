c027c46db1403c1c690bd2bd22d61e385f2b0135
h8d13 <hadean-eon-dev@proton.me>
2026-03-10 13:04:45 +0100

    unused

--- apps/client/src/features/server/coinflip/hooks.ts
+++ apps/client/src/features/server/coinflip/hooks.ts
@@ -1,12 +1,9 @@
 import { useSelector } from 'react-redux';
 import {
     coinflipChallengesSelector,
-    coinflipRecentResultsSelector,
     coinflipTopWinsSelector
 } from './selectors';
 
 export const useCoinflipChallenges = () =>
     useSelector(coinflipChallengesSelector);
-export const useCoinflipRecentResults = () =>
-    useSelector(coinflipRecentResultsSelector);
 export const useCoinflipTopWins = () => useSelector(coinflipTopWinsSelector);
