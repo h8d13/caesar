c07efe43c5de85267b557d8aadfb72b5694671fb
h8d13 <hadean-eon-dev@proton.me>
2026-03-10 13:17:33 +0100

    add progress=plain to start

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@
 # MAGIC RESETS VOLUMES LINE (drop volumes)
 # docker compose down -v && rm -rf data && docker system prune -a --volumes
 # OR just rebuild no cache
-# docker compose --profile prod down && docker system prune && docker compose --profile prod build --no-cache
+# docker compose --profile prod down && docker system prune && docker compose --progress=plain --profile prod build --no-cache
 
 # convenience wrapper for prod builds
 docker compose --profile prod up -d
