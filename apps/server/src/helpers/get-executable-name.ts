6d21874a8ca4568ba12bd09c28ff677f417f7512
H4L0 <hadrien.loge@gmail.com>
2026-05-19 14:54:06 +0200

    more knip

--- apps/server/src/helpers/get-executable-name.ts
+++
@@ -1,9 +0,0 @@
-const getExecutableName = (fileName: string) => {
-  if (process.platform === 'win32') {
-    return `${fileName}.exe`;
-  }
-
-  return fileName;
-};
-
-export { getExecutableName };
