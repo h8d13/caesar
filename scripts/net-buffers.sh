fe1d56cf6dc30b41e7bfd92b0f3966fbf2d2ca18
H4L0 <hadrien.loge@gmail.com>
2026-05-26 19:53:41 +0200

    chore(dev): typo

--- scripts/net-buffers.sh
+++ scripts/net-buffers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 [ "$EUID" -ne 0 ]; echo "this is meant to be run as root" && exit 1
 # one-time setup, as root
-# sets to 16MiB but you can insrease this to 32-64
+# sets to 16MiB but you can increase this to 32-64MiB
 tee /etc/sysctl.d/99-mediasoup.conf > /dev/null <<EOF
 net.core.rmem_max=16777216
 net.core.wmem_max=16777216
